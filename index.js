478b8eb70e4240a81667cff6733715bc7ecd516f
kostiantyn.manko <kostiantyn.manko@guesty.com>
2021-11-22 23:16:43 +0200

    fetch actions and express

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+app.get('/test', (req,res) => {
+  res.send('OK');
+});
+
+app.listen(5000, () => {
+  console.log('Server is running on port 5000');
+});
